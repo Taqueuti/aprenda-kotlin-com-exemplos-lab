d0972f654ea657fb2a47bcf2f296da989e5c8373
Lucas Taqueuti <100800505+Taqueuti@users.noreply.github.com>
2023-09-13 19:35:37 -0300

    Adicionando a solução do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,45 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional> = mutableListOf()) {
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome} foi matriculado na formação $nome")
+        println()
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criar instâncias de ConteudoEducacional
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 60)
+    val conteudo2 = ConteudoEducacional("Estruturas de Controle em Kotlin", 120)
+    val conteudo3 = ConteudoEducacional("Programação Orientada a Objetos em Kotlin", 80)
+
+    // Criar uma formação
+    val formacaoKotlin = Formacao("Formação em Kotlin", mutableListOf())
+
+    // Adicionar conteúdos à formação
+    formacaoKotlin.conteudos.add(conteudo1)
+    formacaoKotlin.conteudos.add(conteudo2)
+    formacaoKotlin.conteudos.add(conteudo3)
+    
+    // Adicionar Alunos inscritos
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+    
+     
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+
+    //Imprimir
+    println("Nome da formação: ${formacaoKotlin.nome}")
+    println("Conteúdos da formação:")
+    formacaoKotlin.conteudos.forEachIndexed { index, conteudo ->
+        println("${index + 1}. ${conteudo.nome} (Duração: ${conteudo.duracao} minutos)")
+    }
 }
